5d976b244b1d5a6934c1af2e433da6af20e6b57e
VincentXie <xierunkun@126.com>
2020-03-27 00:42:58 -0400

    add chap 24

--- 23. Minimum Spanning Trees.py
+++ 23. Minimum Spanning Trees.py
@@ -1,6 +1,8 @@
 """
 Minimum Spanning Trees
 
+Key: Save Edge
+
 Algos:
 1. Kruskal’s Algorithm
 2. Prim’s Algorithm
---
+++ 24. Shortest Paths.py
@@ -0,0 +1,10 @@
+"""
+Shortest Paths (Single Source)
+
+Key: Relaxing
+
+Algos:
+1. Bellmanf-Ford Algo
+2. Dijkstra's Algo
+3. Shortest Path in DAG (Directed Acyclic Graph)
+"""
